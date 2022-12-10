f1d620f8bb1d6b0bbc2f12d4e6c3c1df566be8d8
wb_maqianbo <wb_maqianbo@meituan.com>
2022-12-10 15:55:58 +0800

    webpack多页面打包配置

---
+++ src/pages/home/index.js
@@ -0,0 +1,8 @@
+function render() {
+  const element = document.createElement("div");
+
+  element.innerHTML = "hello home";
+
+  document.body.appendChild(element);
+}
+render();
---
+++ src/pages/list/index.js
@@ -0,0 +1,8 @@
+function render() {
+  const element = document.createElement("div");
+
+  element.innerHTML = "hello list";
+
+  document.body.appendChild(element);
+}
+render();
